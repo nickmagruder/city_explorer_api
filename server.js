dddba2929e85a442468ec345185e9d9125fd89db
nickmagruder <nick.magruder@outlook.com>
2020-12-07 21:13:20 -0800

    user story 2 complete

--- server.js
+++ server.js
@@ -14,21 +14,44 @@ app.use(cors());
 
 
 // ===== light routes ===============================================================
+// https://code301-city-explorer-api.herokuapp.com/location?city=seattle 
 
-/* app.get('/gps', function (req, res) {
-    const gpsData = require('./data/location.json');
-    const instanceOfGpsData = new GpsData(gpsData);
-    res.send(instanceOfGpsData);
 
-    console.log(instanceOfGpsData);
+app.get('/location', function (req, res) {
+    const locationData = require('./data/location.json');
+    const instanceLocationData = new LocationConstructor(locationData[0]);
+    res.send(instanceLocationData);
+    console.log(instanceLocationData);
+});
 
-    res.send(instanceOfGpsData);
-}); */
+
+app.get('/weather', function (req, res) {
+    res.send({
+        "forecast": "Partly cloudy until afternoon.",
+        "time": "Mon Jan 01 2001"
+    }
+    );
+});
+
+
+
+// ===== callback functions ==========================================================
+
+function LocationConstructor(locationObject) {
+    this.search_query = 'Lynnwood';
+    this.formatted_query = locationObject.display_name;
+    this.latitude = locationObject.lat;
+    this.longitude = locationObject.lon;
+}
+
+
+
+
 
 // ===== error handling and server start
 
 app.use('*', (request, response) => {
     response.status(404).send('Route note found');
-  });
-  
-  app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+});
+
+app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
