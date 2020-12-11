3be623b2c7737be46631374e761879ea6f898d03
nickmagruder <nick.magruder@outlook.com>
2020-12-10 21:32:21 -0800

    Lab08 Stories 1,2,3 complete

--- schema.sql
+++ schema.sql
@@ -1,10 +1,19 @@
-DROP TABLE location;
+DROP TABLE IF EXISTS location;
+DROP TABLE IF EXISTS weather_data;
 
 CREATE TABLE location (
-    is SERIAL PRIMARY KEY,
-search_query VARCHAR(255),
-latitude NUMERIC(7, 4);   /*  7 max digits, 4 decimal digits */
-longitude NUMERIC(7, 4);   
-)
+    id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255), -- formatted_query TEXT,  -- showing anothertype here, VARCHAR is better
+    latitude NUMERIC(9, 6),   /*  7 max digits, 4 decimal digits, decimcal and numeric are identical to SQL */
+    longitude NUMERIC(9, 6)   
+);
 
 /* keep the properties the same as constructor */
+
+CREATE TABLE weather_data (
+  id SERIAL PRIMARY KEY,
+  search_query VARCHAR(255),
+  forecast VARCHAR(255),
+  time VARCHAR(255)
+);
